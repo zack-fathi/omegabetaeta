dd13b8f4fd5888a1c4fae10aa5d0f27cb64923c2
Zackery Fathi <zfathi@umich.edu>
2024-05-15 18:02:49 -0400

    updated UI

--- sql/data.sql
+++ sql/data.sql
@@ -1,15 +1,15 @@
 
-INSERT INTO brothers(name, uniqname, fullname, password, line, line_num, lion_name)
-VALUES('zackeryfathi', 'zfathi', 'Zackery Fathi', '1234', 5, 3, 'Aklaf');
+INSERT INTO brothers(name, uniqname, fullname, password, line, line_num, lion_name, active)
+VALUES('zackeryfathi', 'zfathi', 'Zackery Fathi', '1234', 5, 3, 'Aklaf',  1);
 
-INSERT INTO brothers(name, uniqname, fullname, profile_picture, password, line, line_num, lion_name)
-VALUES('jawadalsahlani', 'jawadal', 'Jawad Alsahlani', 'jawad.jpeg', 'pass', 5, 2, 'Muthafar');
+INSERT INTO brothers(name, uniqname, fullname, profile_picture, password, line, line_num, lion_name, active)
+VALUES('jawadalsahlani', 'jawadals', 'Jawad Alsahlani', 'jawad.jpeg', 'pass', 5, 2, 'Muthafar', 1);
 
-INSERT INTO brothers(name, fullname, password, line, line_num, lion_name)
-VALUES('zaidomari', 'Zaid Omari', 'I-like-cock', 5, 10, 'Sarem');
+INSERT INTO brothers(name, fullname, password, line, line_num, lion_name, active)
+VALUES('zaidomari', 'Zaid Omari', 'I-like-cock', 5, 10, 'Sarem', 1);
 
-INSERT INTO brothers(name, fullname, password, line, line_num, lion_name)
-VALUES('ahmadsukhon', 'Ahmad Sukhon', 'pass', 5, 7, 'Baqr');
+INSERT INTO brothers(name, fullname, password, line, line_num, lion_name, active)
+VALUES('ahmadsukhon', 'Ahmad Sukhon', 'pass', 5, 7, 'Baqr', 1);
 
 INSERT INTO brothers(name, fullname, password, line, line_num, lion_name)
 VALUES('jadelharake', 'Jad Elharake', '1234', 0, 1, 'Jad');
